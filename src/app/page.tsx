274fc8897d1e9f96c4283787bbd5c8aa3bdc1029
JoanGuinart <guinartcasasjoan@gmail.com>
2025-10-06 20:01:46 +0200

    fix cms conection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
-import Banner_one from "@/components/Banner_one";
-import Banner_three from "@/components/Banner_three";
-import Banner_two from "@/components/Banner_two";
+import Link from "next/link";
+import { type SanityDocument } from "next-sanity";
 
-export default function Home() {
+import { client } from "@/sanity/client";
+
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function Home() {
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+
   return (
-    <div className="w-full">
-      <h1 className="e1">MAIN PAGE</h1>
-    </div>
+    <main className="container mx-auto min-h-screen max-w-3xl p-8">
+      <h1 className="e1 mb-8">Posts aqui</h1>
+      <ul className="flex flex-col gap-y-4">
+        {posts.map((post) => (
+          <li key={post._id}>
+            {post.slug?.current ? (
+              <Link href={`/${post.slug.current}`}>
+                <h2>{post.title}</h2>
+                <p>
+                  {post.publishedAt &&
+                    new Date(post.publishedAt).toLocaleDateString()}
+                </p>
+              </Link>
+            ) : (
+              <h2>{post.title}</h2>
+            )}
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
